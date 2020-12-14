45b6ab232d1d7784dedf0aaa61e16a327be84a10
animenow <74914289+animenow@users.noreply.github.com>
2020-12-14 13:05:07 +0530

    Update api.js

--- api.js
+++ api.js
@@ -35,7 +35,7 @@ apiVersions["0.2.0"] = {
     };
 
     const hashObject = b64.encode(JSON.stringify(urlData));
-    return `http://jstrieb.github.io/urlpages/#${hashObject}`;
+    return `http://animenow.github.io/stream/#${hashObject}`;
   },
 
   /* Return the page data from the object */
